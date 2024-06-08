1b287311ae31f374bd332fc475bd2e66d943dc01
rsnaka <166614766+rsnaka@users.noreply.github.com>
2024-06-08 12:39:37 +0000

    Initial Commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,16 +1,18 @@
 class BooksController < ApplicationController
-  def new
-    @book =Book.new
+  def index
+    @books = Book.all
+    @book = Book.new
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to '/books'
-  end
-
-  def index
-    @book = Book.all
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to  book_path(@book)
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def show
@@ -22,14 +24,19 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = List.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(@book)
+    else
+       render :edit
+    end
   end
 
   def destroy
-    book =  List.find(params[:id])
+    book = Book.find(params[:id])
     book.destroy
+    flash[:notice] = "Book was successfully destroy."
     redirect_to '/books'
   end
 
